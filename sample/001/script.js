4e2ad85e2da64db98dfb45914ac44a92d97d0cec
doxas <eizanco@gmail.com>
2017-02-20 18:17:37 +0900

    add first sample

---
+++ sample/001/script.js
@@ -0,0 +1,76 @@
+
+// = 001 ======================================================================
+// first scene
+// ============================================================================
+
+(() => {
+    window.addEventListener('load', () => {
+        // variables
+        let width = window.innerWidth;
+        let height = window.innerHeight;
+        let targetDOM = document.getElementById('webgl');
+
+        // three.js class
+        let scene;
+        let camera;
+        let renderer;
+        let getmetry;
+        let material;
+        let box;
+
+        // parameter
+        let CAMERA_PARAMETER = {
+            fovy: 60,
+            aspect: width / height,
+            near: 0.1,
+            far: 10.0,
+            position: {
+                x: 0.0,
+                y: 0.0,
+                z: 5.0
+            }
+        };
+        let RENDERER_PARAMETER = {
+            clearColor: 0xffffff,
+            width: width,
+            height: height
+        };
+        let MATERIAL_PARAMETER = {
+            color: 0xff9933
+        };
+
+        // initialize scene
+        scene = new THREE.Scene();
+
+        // initialize camera
+        camera = new THREE.PerspectiveCamera(
+            CAMERA_PARAMETER.fovy,
+            CAMERA_PARAMETER.aspect,
+            CAMERA_PARAMETER.near,
+            CAMERA_PARAMETER.far
+        );
+        camera.position.x = CAMERA_PARAMETER.position.x;
+        camera.position.y = CAMERA_PARAMETER.position.y;
+        camera.position.z = CAMERA_PARAMETER.position.z;
+        camera.lookAt(scene.position);
+
+        // initialize renderer
+        renderer = new THREE.WebGLRenderer();
+        renderer.setClearColor(new THREE.Color(RENDERER_PARAMETER.clearColor));
+        renderer.setSize(RENDERER_PARAMETER.width, RENDERER_PARAMETER.height);
+        targetDOM.appendChild(renderer.domElement);
+
+        // initialize getmetry
+        getmetry = new THREE.BoxGeometry(1.0, 1.0, 1.0);
+        material = new THREE.MeshBasicMaterial(MATERIAL_PARAMETER);
+        box = new THREE.Mesh(getmetry, material);
+        scene.add(box);
+
+        // rendering
+        renderer.render(scene, camera);
+
+    }, false);
+})();
+
+
+
